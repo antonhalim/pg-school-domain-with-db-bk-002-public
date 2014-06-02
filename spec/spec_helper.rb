644b0b74f7bb591596c915ab34b7b761e777b8b5
Steven Nunez <steven.nunez@gmail.com>
2014-06-02 14:48:27 -0400

    Drop Tables before running tests

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,7 +5,7 @@ DB[:conn] = PG.connect(dbname: 'school_domain_test')
 
 RSpec.configure do |config|
   # Use color in STDOUT
-  config.color_enabled = true
+  config.color = true
 
   # Use color not only in STDOUT but also in pagers and files
   config.tty = true
@@ -16,8 +16,10 @@ RSpec.configure do |config|
   #you can do global before/after here like this:
   config.before(:each) do
     if Student.respond_to?(:create_table)
+      Student.drop_table
       Student.create_table
     else
+      DB[:conn].exec("DROP TABLE IF EXISTS students")
       DB[:conn].exec("CREATE TABLE IF NOT EXISTS students (id SERIAL PRIMARY KEY, name TEXT, tagline TEXT, github TEXT, twitter TEXT, blog_url TEXT, image_url TEXT, biography TEXT)")
     end
   end
